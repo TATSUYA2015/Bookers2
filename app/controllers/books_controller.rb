e552373c9ca5f8cf20f0aa31338d91d1a2f8dc61
EC2 Default User <112598954+TATSUYA2015@users.noreply.github.com>
2022-10-09 14:16:02 +0000

    [add]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,7 +34,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
-
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
   resources :books, only:[:create, :index, :show, :edit, :update, :destroy]
   resources :users, only:[:index, :show, :edit, :update]
