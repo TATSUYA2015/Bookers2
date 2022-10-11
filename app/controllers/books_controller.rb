a319f280faf385790c647d5f6336969209cc9f09
EC2 Default User <112598954+TATSUYA2015@users.noreply.github.com>
2022-10-11 13:21:38 +0000

    [add]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,13 @@ class BooksController < ApplicationController
 
   def create
     @book=Book.new(book_params)
+    @books=Book.all
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+     redirect_to book_path(@book.id)
+    else
+     render :index
+    end
   end
 
   def show
@@ -42,6 +46,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-   params.require(:book).permit(:title, :body, :user_id)
+   params.require(:book).permit(:title, :body, :user_id, :profile_image )
   end
 end
